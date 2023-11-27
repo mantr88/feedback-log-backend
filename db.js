c03f22058d8bb836a9e31e7175894f6cf256c397
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-11-27 21:39:44 +0200

    fix: db connection data

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 import mysql from 'mysql';
+const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1',
-  database: 'feedbackLog',
+  host: DB_HOST,
+  port: 3306,
+  user: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
 });
